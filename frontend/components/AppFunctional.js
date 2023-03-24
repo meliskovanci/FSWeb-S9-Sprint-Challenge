d2adc3719b31b48dd88d81912651e13733d9124f
Melis Kovancı <meliskovanci@hotmail.com>
2023-03-24 23:20:57 +0300

    test kaldı

--- frontend/components/AppFunctional.js
+++ frontend/components/AppFunctional.js
@@ -1,4 +1,6 @@
-import React from 'react'
+import React, {useState} from 'react'
+import axios from 'axios';
+
 
 // önerilen başlangıç stateleri
 const initialMessage = ''
@@ -6,71 +8,150 @@ const initialEmail = ''
 const initialSteps = 0
 const initialIndex = 4 //  "B" nin bulunduğu indexi
 
+
+
 export default function AppFunctional(props) {
   // AŞAĞIDAKİ HELPERLAR SADECE ÖNERİDİR.
   // Bunları silip kendi mantığınızla sıfırdan geliştirebilirsiniz.
 
-  function getXY() {
+  const initialGrid = [
+    [1, 1], [2, 1], [3, 1],
+    [1, 2], [2, 2], [3, 2],
+    [1, 3], [2, 3], [3, 3],
+  ]
+
+
+  const [steps, setSteps] = useState(initialSteps)
+  const [message, setMessage] = useState(initialMessage)
+  const [email, setEmail] = useState(initialEmail)
+  const [index, setIndex] = useState(initialIndex)
+
+  
+ function getXY(initialGrid,index) {
     // Koordinatları izlemek için bir state e sahip olmak gerekli değildir.
     // Bunları hesaplayabilmek için "B" nin hangi indexte olduğunu bilmek yeterlidir.
+     return [initialGrid[index][0], initialGrid[index][1]]
   }
 
-  function getXYMesaj() {
+  // const [x,y]=getXY(initialGrid,index)
+  // console.log(x,y)
+
+  function getXYMesaj(getXY,initialGrid,index,steps) {
     // Kullanıcı için "Koordinatlar (2, 2)" mesajını izlemek için bir state'in olması gerekli değildir.
     // Koordinatları almak için yukarıdaki "getXY" helperını ve ardından "getXYMesaj"ı kullanabilirsiniz.
     // tamamen oluşturulmuş stringi döndürür.
+    
+    // return `(${getXY(initialGrid)[index][0]}, ${getXY(initialGrid)[index][1]})`
+   const text=[];
+   const [x,y]=getXY(initialGrid,index);
+   text[0]=`(${x},${y})`;
+   text[1]=steps;
+   return text;
+
+   
   }
 
   function reset() {
     // Tüm stateleri başlangıç ​​değerlerine sıfırlamak için bu helperı kullanın.
+    setSteps(initialSteps)
+    setIndex(initialIndex)
+    setEmail(initialEmail)
+    setMessage(initialMessage)
+    
   }
 
   function sonrakiIndex(yon) {
     // Bu helper bir yön ("sol", "yukarı", vb.) alır ve "B" nin bir sonraki indeksinin ne olduğunu hesaplar.
     // Gridin kenarına ulaşıldığında başka gidecek yer olmadığı için,
     // şu anki indeksi değiştirmemeli.
+
+    if(yon === 'sol' && index !==0 && index !==3 && index !==6) {
+      setIndex(index - 1)
+      setSteps(steps + 1)
+    
+  } else if (yon === 'sağ' && index !==2 && index !==5 && index !==8) {
+      setIndex(index + 1)
+      setSteps(steps +1)
+    
+  } else if (yon === 'aşağı' && index !==6 && index !==7 && index !==8) {
+      setIndex(index + 3)
+      setSteps(steps + 1)
+  } else if(yon === 'yukarı' && index !==0 && index !== 1 && index !== 2) {
+      setIndex(index - 3)
+      setSteps(steps +1)
+  } 
+  else {
+      setMessage(`${yon} a gidilmiyor!`)
   }
+  }
+  
 
   function ilerle(evt) {
     // Bu event handler, "B" için yeni bir dizin elde etmek üzere yukarıdaki yardımcıyı kullanabilir,
     // ve buna göre state i değiştirir.
+  
   }
 
   function onChange(evt) {
     // inputun değerini güncellemek için bunu kullanabilirsiniz
+    setEmail(evt.target.value)
   }
 
   function onSubmit(evt) {
     // payloadu POST etmek için bir submit handlera da ihtiyacınız var.
-  }
+    evt.preventDefault(evt)
 
+
+    const payLoad = {
+    "x":initialGrid[index][0], 
+    "y":initialGrid[index][1], 
+    "steps":steps, 
+    "email":email}
+
+    axios
+    .post( "http://localhost:9000/api/result" ,payLoad)
+    .then(res=>
+      // console.log(res.data)
+      setMessage(res.data.message))
+    .catch(error => setMessage(error.res.data.message))
+  
+    setEmail(" ")
+
+  }
+    
+  
   return (
     <div id="wrapper" className={props.className}>
       <div className="info">
-        <h3 id="coordinates">Koordinatlar (2, 2)</h3>
-        <h3 id="steps">0 kere ilerlediniz</h3>
+        <h3 id="coordinates">Koordinatlar {getXYMesaj(getXY,initialGrid,index,steps)[0]} </h3>
+        
+        <h3 id="steps">{getXYMesaj(getXY,initialGrid,index,steps)[1]} kere ilerlediniz</h3>
+
       </div>
       <div id="grid">
         {
           [0, 1, 2, 3, 4, 5, 6, 7, 8].map(idx => (
-            <div key={idx} className={`square${idx === 4 ? ' active' : ''}`}>
-              {idx === 4 ? 'B' : null}
+
+            <div key={idx} className={`square${idx === index ? ' active' : ''}`}>
+
+
+              {idx === index ? 'B' : null}
             </div>
           ))
         }
       </div>
       <div className="info">
-        <h3 id="message"></h3>
+        <h3 id="message">{message}</h3>
       </div>
       <div id="keypad">
-        <button id="left">SOL</button>
-        <button id="up">YUKARI</button>
-        <button id="right">SAĞ</button>
-        <button id="down">AŞAĞI</button>
-        <button id="reset">reset</button>
+        <button onClick={()=>sonrakiIndex("sol")} id="left">SOL</button>
+        <button onClick={()=>sonrakiIndex("yukarı")} id="up">YUKARI</button>
+        <button onClick={()=>sonrakiIndex("sağ")} id="right">SAĞ</button>
+        <button onClick={()=>sonrakiIndex("aşağı")} id="down">AŞAĞI</button>
+        <button onClick={reset} id="reset">reset</button>
       </div>
-      <form>
-        <input id="email" type="email" placeholder="email girin"></input>
+      <form onSubmit= {onSubmit}>
+        <input onChange={onChange} id="email" type="email" placeholder="email girin"></input>
         <input id="submit" type="submit"></input>
       </form>
     </div>
